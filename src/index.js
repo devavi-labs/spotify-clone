71f7d9ac5e20854e57b2d0ad9a650fae0acceb8f
Avinash Sinha <savinash2608@gmail.com>
2020-09-15 13:44:19 +0530

    master

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import DataLayer from './data_layer'
 import reducer, { initialState } from './reducer';
